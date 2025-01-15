be321743640d9d3597e1d8c413a0b352e95115dc
Bálintfy Péter <balintfy.peter@burg.lo.hu>
2025-01-15 10:09:32 +0100

    feladat1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import ProductCard from "./ProductCard";
 import "./App.css";
+import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
     <div>
       <h1>Product Information</h1>
-      <ProductCard />
+      <ProductCard></ProductCard>
     </div>
   );
 }
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,16 @@
+export type Product = {
+    id: number,
+    name: string,
+    price: number,
+    category: string,
+    image: string
+}
+
+
+const ProductCard = () => {
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
