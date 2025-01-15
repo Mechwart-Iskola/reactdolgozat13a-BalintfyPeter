21df69d72de15b160916e06de634d5de4eeac7b7
Bálintfy Péter <balintfy.peter@burg.lo.hu>
2025-01-15 10:34:54 +0100

    :((

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react"
+import img from '../../public/products.json'
+
 export type Product = {
     id: number,
     name: string,
@@ -6,10 +9,22 @@ export type Product = {
     image: string
 }
 
+const [images, setImages] = useState();
+
+useEffect(() => {
+    let path = "../../public/products.json"
+    fetch(path)
+        .then(res => res.json())
+        .then(images =>setImages(images))
+}, [])
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <>
+        <div>{images.id.map((Image) =>{
+            <div key={images.id} {...images}></div>
+        }}</div>
+    </>
   )
 }
 
